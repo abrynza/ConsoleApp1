9b495bc48a99f0834dc1802f6d465a8a41dc50ce
abrynza <abrynza@gmail.com>
2017-09-02 18:01:49 -0500

    Add project files.

---
+++ ConsoleApp1/CaseInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class CaseInfo
+    {
+        public int Id
+        { get; set; }
+
+        public string Details
+        { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            List<CaseInfo> caseList = new List<CaseInfo>();
+            for(int ii = 0; ii < 10000; ii++)
+            {
+                caseList.Add(new CaseInfo { Id = ii, Details = (ii + ii).ToString() });
+            }
+
+            IEnumerable<CaseInfo> query = from item in caseList
+                                          where item.Id % 2 == 0
+                                          select item;
+
+            foreach (var item in query)
+                Console.WriteLine("caseId:" + item.Id + ", caseDetails:" + item.Details);
+        }
+    }
+}
